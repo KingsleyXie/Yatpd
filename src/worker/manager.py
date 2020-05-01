f5c1ebf68b1176bc8bf059dbc7a5acb36b50e444
Kingsley <kingsley.xym@gmail.com>
2020-05-01 23:50:31 +0800

    :sparkles: Add Runable Master-Worker Code

--- src/worker/manager.py
+++ src/worker/manager.py
@@ -3,31 +3,56 @@
 
 import os
 import signal
+from setproctitle import setproctitle
 
 class Manager(object):
     def __init__(self):
         self.worker_cnt = 0
         self.curr_worker = 0
-        signal.signal(signal.SIGTTIN, self.check(1))
-        signal.signal(signal.SIGTTOU, self.check(-1))
-        signal.signal(signal.SIGCHLD, self.killzomb())
+        self.chld_set = 0
+        setproctitle('Yatpd - Master')
+        signal.signal(signal.SIGTTIN, self.check)
+        signal.signal(signal.SIGTTOU, self.check)
+        while True:
+            pass
 
-    def check(self, step):
-        self.worker_cnt += step
+
+    def check(self, sig, frame):
+        if sig == signal.SIGTTIN:
+            self.worker_cnt += 1
+        elif sig == signal.SIGTTOU:
+            self.worker_cnt -= 1
+        else:
+            return
+
         if self.worker_cnt > self.curr_worker:
-            self.delworkers()
-        elif self.worker_cnt < self.curr_worker:
             self.addworkers()
+        elif self.worker_cnt < self.curr_worker:
+            self.delworkers()
+
 
     def addworkers(self):
         offset = self.worker_cnt - self.curr_worker
         for i in range(offset):
             self.curr_worker += 1
-            if os.fork() != 0:
+            if os.fork() == 0:
+                setproctitle(f'Yatpd - Worker #{self.curr_worker}')
+            else:
+                if not self.chld_set:
+                    signal.signal(signal.SIGCHLD, self.killzomb)
+                    self.chld_set = 1
                 return
 
-    def delworkers(self):
-        os.kill(-1)
 
-    def killzomb(self):
+    def delworkers(self):
+        pass
+        # os.kill(-1, signal.SIGKILL)
+
+
+    def killzomb(self, sig, frame):
+        print('killzomb start')
         os.waitpid(-1, os.WNOHANG)
+        print('killzomb end')
+
+
+m = Manager()
